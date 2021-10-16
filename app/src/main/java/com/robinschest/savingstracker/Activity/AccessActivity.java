0d37e0fcf8fcbe44a5b3c08893515cf31da85396
loverofMush <tolujamal1@gmail.com>
2021-10-16 01:02:23 +0100

    Initial commit

---
+++ app/src/main/java/com/robinschest/savingstracker/Activity/AccessActivity.java
@@ -0,0 +1,16 @@
+package com.robinschest.savingstracker.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.robinschest.savingstracker.R;
+
+public class AccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access);
+    }
+}
