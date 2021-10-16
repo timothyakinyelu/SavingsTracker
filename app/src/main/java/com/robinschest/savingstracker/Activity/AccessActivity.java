6a1cd8ab1429e082a614e99dede1063ea442200b
mainlogixsoftware <info@mainlogix.org>
2021-10-16 19:33:26 +0100

    - init view elements - added method to validate fields - added authenticate method

--- app/src/main/java/com/robinschest/savingstracker/Activity/AccessActivity.java
+++ app/src/main/java/com/robinschest/savingstracker/Activity/AccessActivity.java
@@ -1,29 +1,88 @@
 package com.robinschest.savingstracker.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.robinschest.savingstracker.R;
 
 
 public class AccessActivity extends AppCompatActivity {
+    private EditText editUsername, editPin;
+    private TextView warnUsername, warnPin;
     private CheckBox pinCheckBox;
-    private EditText editPin;
+    private ConstraintLayout parentLayout;
+    private AppCompatButton enterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access);
 
+        initElements();
+
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticate();
+            }
+        });
+        onCheckBoxClick();
+    }
+
+    private void authenticate() {
+        if(validateData()) {
+            allowEntry();
+        }
+    }
+
+    private void allowEntry() {
+        warnUsername.setVisibility(View.GONE);
+        warnPin.setVisibility(View.GONE);
+
+        Snackbar.make(parentLayout, "Access Granted", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+    }
+
+    private boolean validateData() {
+        if(editUsername.getText().toString().equals("")) {
+            warnUsername.setVisibility(View.VISIBLE);
+            warnUsername.setText("Please enter a username");
+            return false;
+        }
+
+        if(pinCheckBox.isChecked()) {
+            if(editPin.getText().toString().equals("")) {
+                warnPin.setVisibility(View.VISIBLE);
+                warnPin.setText("Enter a 4-DIGIT PIN");
+                return  false;
+            }
+        }
+        return true;
+    }
+
+    private void initElements() {
         pinCheckBox = findViewById(R.id.setPin);
         editPin = findViewById(R.id.editPin);
-        onCheckBoxClick();
+        editUsername = findViewById(R.id.editUsername);
+        parentLayout = findViewById(R.id.parentLayout);
+        enterBtn = findViewById(R.id.enterBtn);
+        warnUsername = findViewById(R.id.warnUsername);
+        warnPin = findViewById(R.id.warnPin);
     }
 
     public void onCheckBoxClick() {
