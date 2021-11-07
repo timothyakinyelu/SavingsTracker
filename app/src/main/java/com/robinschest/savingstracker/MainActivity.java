75edfaac41bfb388a367706ad1c77893dc22502d
timothyakinyelu <tolujamal1@gmail.com>
2021-11-07 18:47:58 +0100

    - added bottom navigation

--- app/src/main/java/com/robinschest/savingstracker/MainActivity.java
+++ app/src/main/java/com/robinschest/savingstracker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.robinschest.savingstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.app.Activity;
 import android.content.Context;
@@ -8,12 +11,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setBackground(null);
+        bottomNav.getMenu().getItem(2).setEnabled(false);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
     public static void hideSoftKeyBoard(View view, Activity activity) {
